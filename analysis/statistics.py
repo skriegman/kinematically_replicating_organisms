24c3a1a7cf7668e9789cbbe34adbbadffb8232c7
Sam Kriegman <sam.kriegman@uvm.edu>
2021-04-28 19:13:19 -0400

    Add files via upload

--- analysis/statistics.py
+++ analysis/statistics.py
@@ -30,10 +30,10 @@ print(len(s), len(c))
 
 print(mannwhitneyu(s,c))
 
-s = [1, 1, 2, 1, 1]
-c = [3, 2, 4]
+gs = [1, 1, 2, 1, 1]
+gc = [3, 2, 4]
 
-print(mannwhitneyu(s,c))
+print(mannwhitneyu(gs,gc))
 
 print("fdr: ", multitest.fdrcorrection([3.9e-5, 1.2e-7, 0.018817, 1e-7], alpha=0.05, method='indep', is_sorted=False))
 
@@ -59,7 +59,7 @@ from matplotlib.lines import Line2D
 
 size = np.array(x4_c61 + x7_c61 + x8_c91 + x1_s25 + x5_s2x52 + x2_s75 + x6_sflat105 + x3_s150)
 cells = np.array([61.,]*10 + [61.,]*10 + [91.,]*10 + [25.,]*10 + [52.,]*10 + [75.,]*10 + [105.,]*10 + [150.,]*10)
-shape = np.array(["torus",]*10 + ["torus",]*10 + ["torus",]*10 + ["sphere",]*10 + ["sphere",]*10 + ["large sphere",]*10 + ["compressed",]*10 + ["sphere",]*10)
+shape = np.array(["semi torus",]*10 + ["semi torus",]*10 + ["semi torus",]*10 + ["sphere",]*10 + ["sphere",]*10 + ["large sphere",]*10 + ["compressed",]*10 + ["sphere",]*10)
 
 df = pd.DataFrame(np.array([size, cells, shape]).T)
 df.columns = ['size', 'cells', 'shape']
@@ -75,32 +75,33 @@ sns.despine(top=True, right=True)
 
 ax.set_xlim([0, 175])
 
-sns.regplot(x=df["cells"][df["shape"] != "torus"], y=df["size"][df["shape"] != "torus"],
+sns.regplot(x=df["cells"][df["shape"] != "semi torus"], y=df["size"][df["shape"] != "semi torus"],
             scatter=False,
             logx=True,
             truncate=False,
-            ci=95, color=(.5, .5, .5), line_kws={"ls": "--", "lw": 1})
+            ci=95, color=(.5, .5, .5), line_kws={"ls": "--", "lw": 1, "zorder": 0})
 
 # Show each observation with a scatterplot
-palette={"torus": (1, 0.0, 1, 0.6),
-         "sphere": (0, 0, 0, 0.6),
-         "large sphere": (.5, .5, .5, 0.4),
-         "compressed": (1, 1, 1, 0.2)}
+palette={"semi torus": (1, 0.0, 1, 1),
+         "sphere": (0.2, 0.2, 0.2, 1),
+         "large sphere": (.6, .6, .6, 1),
+         "compressed": (1, 1, 1, 1)}
 sns.scatterplot(x="cells", y="size", hue="shape", data=df, zorder=1,
                 # marker=TextPath((0,0), "c"),
                 edgecolor=(0, 0, 0, 1),
+                s=100,
                 palette=palette
                 )
 
 # Improve the legend
 # legend_elements = [Circle((0,0), facecolor='orange', edgecolor='r', label='asdf')]
 handles, labels = ax.get_legend_handles_labels()
-new_handles = [Line2D([0], [0], marker='o', markerfacecolor=palette["torus"], markeredgecolor='black', markersize=8, ls=''),
-               Line2D([0], [0], marker='o', markerfacecolor=palette["sphere"], markeredgecolor='black', markersize=8, ls=''),
-               Line2D([0], [0], marker='o', markerfacecolor=palette["large sphere"], markeredgecolor='black', markersize=8, ls=''),
-               Line2D([0], [0], marker='o', markerfacecolor=palette["compressed"], markeredgecolor='black', markersize=8, ls=''),
+new_handles = [Line2D([0], [0], marker='o', markerfacecolor=palette["semi torus"], markeredgecolor='black', markersize=10, ls=''),
+               Line2D([0], [0], marker='o', markerfacecolor=palette["sphere"], markeredgecolor='black', markersize=10, ls=''),
+               Line2D([0], [0], marker='o', markerfacecolor=palette["large sphere"], markeredgecolor='black', markersize=10, ls=''),
+               Line2D([0], [0], marker='o', markerfacecolor=palette["compressed"], markeredgecolor='black', markersize=10, ls=''),
                ]
-ax.legend(new_handles, labels, loc="upper left")
+ax.legend(new_handles, labels, loc=(0.89, 0.77), fontsize=12)
 # ax.legend(title=None)
 
 ax.set_xlim([0, 175])
@@ -110,14 +111,42 @@ ax.set_ylabel('Diameter of 10 largest offspring (mm)', fontsize=16)
 ax.set_xlabel('Dissociated stem cell density (cells/mm$^2$)', fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-axins = inset_axes(ax, "30%", "30%", loc="upper right", borderpad=-5)
+axins = inset_axes(ax, "100%", "100%", bbox_to_anchor=(1.03, 0.07, .3, .3), bbox_transform=ax.transAxes)
+
+none61 = [0.075905263, 0.071828947, 0.071052632, 0.066418421, 0.064513158, 0.064084211, 0.063157895, 0.061434211, 0.061434211, 0.061378947]
+none83 = [0.073165789, 0.071102632, 0.063268421, 0.059136842, 0.057894737, 0.053673684, 0.052697368, 0.049652632, 0.047442105, 0.047442105]
+none150 = [0.078242105, 0.074105263, 0.072260526, 0.069526316, 0.067247368, 0.066989474, 0.065789474, 0.059663158, 0.058431579, 0.058431579]
+
+n = np.array(none61+none83+none150)
+n /= np.array([61.,]*10 + [83.,]*10 + [150.,]*10)
+ratios = np.append(s.copy(), c.copy(), axis=0)
+ratios = np.append(n.copy(), ratios.copy(), axis=0) * 1000
+labels = np.array(["n" for i in n] + ["s" for j in s] + ["t" for k in c])
+df2 = pd.DataFrame(np.array([ratios, labels]).T)
+df2.columns = ['ratios', 'labels']
+df2["ratios"] = pd.to_numeric(df2["ratios"], downcast="float")
+
+sns.barplot(x='labels', y='ratios', data=df2, ax=axins, errwidth=1, capsize=0.1, ci=95,
+            palette=[(0, 1, 0), (.5, .5, .5), palette["semi torus"]], saturation=0.75, alpha=0.7)
+
+# axins.text(0, 1, r"$\it{none}$", ha="center", va="bottom", size=10)
+axins.text(0, 1.2, r"$\it{cells \; only}$", ha="center", va="bottom", rotation=90, size=11)
+# axins.text(1, 0.25, "spheres", ha="center", va="bottom", rotation=90, size=10)
+# axins.text(2, 0.25, "semi tori", ha="center", va="bottom", rotation=90, size=10)
+
 axins.set_ylim([0, 6])
-axins.set_xticks(range(1, 7, 1))
-axins.set_xlim([0, 4])
-axins.set_xticks([1,2,3])
-axins.set_ylabel(r'$\frac{\mathrm{offspring \;\; diameter}}{\mathrm{dissociated \;\; cell \;\; density}}$', fontsize=16)
-axins.set_xlabel('Progenitors', fontsize=12)
-axins.tick_params(axis='both', which='major', labelsize=12)
+# axins.set_yticks([0, 0.002, 0.])
+# axins.set_yticklabels([])
+# axins.set_xticks(range(1, 7, 1))
+# axins.set_xlim([0, 4])
+# axins.set_xticks([1,2,3])
+axins.set_xticklabels([])
+# axins.set_ylabel(r'$\frac{\mathrm{diameter}}{\mathrm{cell \;\, density}}$', fontsize=14)
+axins.set_ylabel('diameter / cell density', fontsize=11)
+axins.set_xlabel('')
+# axins.set_xlabel('Progenitors', fontsize=11)
+# axins.set_xticklabels(['none', 'o', 'c'])
+axins.tick_params(axis='both', which='major', labelsize=10)
 
 
 f.subplots_adjust(wspace=-0.05, hspace=-0.05)
